5504eddc5e35da829c1d8416a8ed74b0eb106511
Eoghan Malone <eoghan.malone@gmail.com>
2018-03-02 17:32:37 +0000

    First push

---
+++ src/HCIAssignment2.java
@@ -0,0 +1,18 @@
+
+/**
+ * HCI Assignment 2 - HUD display
+ * 
+ * @author OUR TEAM NAME
+ *
+ */
+public class HCIAssignment2 {
+	
+	
+	public static void main (String [] args) {
+		
+		System.out.print("YOLO world");
+		
+		
+	}
+
+}
---
+++ src/HUD.java
@@ -0,0 +1,50 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * HUD DISPLAY
+ * @author 2227056m
+ *
+ */
+public class HUD extends JFrame implements ActionListener {
+
+	// LOCALS
+	
+	
+	public HUD() {
+		
+		// Init the HUD
+		
+	}
+	
+	
+	
+	
+	
+	
+	/**
+	 * Controller
+	 */
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		// Regroup with Lewis and establish controller movements
+		
+		// left
+		
+		// right
+		
+		// up
+		
+		// down
+		
+		
+	
+	}
+
+	
+	
+}

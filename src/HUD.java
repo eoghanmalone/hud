3e439c38147f523780aa44f35eea4f90ad344bdb
Eoghan Malone <eoghan.malone@gmail.com>
2018-03-02 18:43:26 +0000

    HUD backround and decoration killed

--- src/HUD.java
+++ src/HUD.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -13,17 +14,20 @@ import javax.swing.JFrame;
 public class HUD extends JFrame implements ActionListener {
 
 	// LOCALS
+	private int hudWidth, hudHeight;
 	
 	
 	public HUD() {
 		
 		// Init the HUD
-		System.out.print("HUD");
+		hudHeight = hudWidth = 500;
 		frameHUD();
 		
 	}
 	
     /*
+     * Scratchpad
+     * 
 	    BufferedImage buttonIcon = ImageIO.read(new File("myImage.png"));
 	    button = new JButton(new ImageIcon(buttonIcon));
 	    button.setBorderPainted(false);
@@ -31,8 +35,6 @@ public class HUD extends JFrame implements ActionListener {
 	    button.setContentAreaFilled(false);
     */
 
-
-	
 	public void frameHUD() {
 		
 		JFrame frame = new JFrame("HUD");
@@ -45,9 +47,13 @@ public class HUD extends JFrame implements ActionListener {
         int x = (dim.width-w)/2-250;
         int y = ((dim.height-h)/2);
 		
+        // Disappear the background
+        frame.setUndecorated(true);
+        frame.getContentPane().setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
+        frame.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
 		
-		frame.pack();
-		frame.setSize(500, 500);
+		frame.pack(); // take pack off later. Might be too bunched
+		frame.setSize(hudWidth, hudHeight);
 		frame.setLocation(x,y);
 		frame.setVisible(true);
 	}	
